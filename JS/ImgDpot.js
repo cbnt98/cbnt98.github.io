ecbc0fdd05d07df5c15c860679e2f8eaa4ba9396
cbnt98 <151330960+cbnt98@users.noreply.github.com>
2023-11-29 15:50:24 +0900

    Add files via upload

--- JS/ImgDpot.js
+++ JS/ImgDpot.js
@@ -15,13 +15,21 @@ var contentImgs = [
     new imageClass(1,0,"熊本旅行中、阿蘇","2023-3"),
     new imageClass(1,1,"EPFL コミュニティーラーニングセンター、スイス　ローザンヌ", "2023-9"),
     new imageClass(1,2,"自宅","2023-11"),
+    new imageClass(2,0,"","2023-9"),
+    new imageClass(2,1,"","2023-9"),
+    new imageClass(2,2,"","2023-9"),
+    new imageClass(2,3,"","2023-9"),
+    new imageClass(2,4,"","2023-9"),
+    new imageClass(2,5,"","2023-9"),
+    new imageClass(2,6,"","2023-9"),
+
     // new imageClass(0,0,"","")
 ];
 
 // あるページの画像を全て、配置する。
 function WriteImage(x){
     var pageImgs = contentImgs.filter(i => i.pageNo === x);
-    for (let i = 0; i < pageImgs.length; i++){
+    for (let i = 1; i < pageImgs.length; i++){
         var img = document.createElement('img');
         img.setAttribute('src','Img/' + x + '-' + i + '.jpg');
         img.setAttribute('class', 'contentImg');
@@ -30,7 +38,7 @@ function WriteImage(x){
         var p = document.createElement('p');
         p.setAttribute('class','imgComment');
 
-        var targetImg = contentImgs.find(h => h.imgNo === i);
+        var targetImg = pageImgs.find(h => h.imgNo === i);
         p.textContent = targetImg.comment + ' <' +  targetImg.date + ">";
 
         document.getElementById('Img'+x+'-'+i).appendChild(img)
