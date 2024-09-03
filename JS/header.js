314cb59fbae496092832d64797b8955c991d5232
cbnt98 <151330960+cbnt98@users.noreply.github.com>
2024-09-03 16:49:01 +0900

    Update header.js

--- JS/header.js
+++ JS/header.js
@@ -1,14 +1,64 @@
-function writeHeader(){
-    document.write(header);
+const BlogName = 'Cabinet (キャビネット)';
+const BlogSubText = '家具的なブログ..';
+
+class pageClass{
+  constructor(title,file){
+    this.title = title;
+    this.file = file;
+  }
 }
+
+var HeaderList = [
+  new pageClass('Top','index.html'),
+  new pageClass('Splendid Ocean Blue','movies.html'),
+  new pageClass('This is the way I like it','page.html'),
+  new pageClass('Cabinet','books.html'),
+  new pageClass('abaut','about.html'),
+]
+
+
+function writeHeader(){
+    var Header = document.getElementById('header');
+    // var Header = document.createElement('header');
+    // Header.setAttribute('id', 'header');
+
+    var Logo = document.createElement('div');
+    Logo.setAttribute('class', 'logo');
+    Logo.textContent = BlogName;
+    Header.appendChild(Logo);
+
+    var SubLogo = document.createElement('div');
+    SubLogo.setAttribute('class', 'sublogo');
+    SubLogo.textContent = BlogSubText;
+
+    var Ul = document.createElement('ul');
+    Ul.setAttribute('id','hederList')
+
+    for(i = HeaderList.length - 1; i>=0; i--){
+      var Li = document.createElement('li');
+      Li.setAttribute('id','headerLIst');
+
+      var A = document.createElement('a');
+      A.setAttribute('href',HeaderList[i].file);
+      A.textContent = HeaderList[i].title;
+
+      Li.appendChild(A);
+      Ul.appendChild(Li);
+    }
+}
+
+
+
+
+
 
 var header = `
 <header id="header">
-<!-- TODO:  Use a tags instead of ul -->
 
 <div class="logo">Cabinet (キャビネット)</div>
 <div class="sublogo">家具的なブログ..</div>
-<nav class="menu">
+
+
   <ul id= "headerList">
     <li id = "headerList">
       <a href="index.html">
@@ -35,7 +85,7 @@ var header = `
     </li>
     
   </ul>
-</nav>
+
 
 </header>
 `
